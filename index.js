3f90e3dcadecf1703e40cd71f2327cf31b1be625
VivianOAlmeida <oliveiraalmeidavivian@gmail.com>
2026-06-01 12:27:38 -0300

    adiciona mapa da empatia do projeto e corrige erro de sintaxe no javascript relacionado às imagens do resultado do formulário

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ const galeria = [
     "img/Desvalorização.png",
     "img/Valorização2.png",
     "img/Importância.png",
-    "img/Sugestões.png",
+    "img/Sujestões.png",
     "img/Visão Geral.png"
 ];
 
